5056f7fa91601e9107ad4972a6808f2142836d5f
Linruoxin-Sy <342972612@qq.com>
2025-09-18 23:21:43 +0800

    feat: use rollup for package

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+import json from '@rollup/plugin-json';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    { file: 'dist/index.cjs.js', format: 'cjs', sourcemap: true },
+    { file: 'dist/index.esm.js', format: 'esm', sourcemap: true },
+    { file: 'dist/index.umd.js', format: 'umd', name: 'MyLibrary', sourcemap: true, plugins: [terser()] }
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript(),
+    json()
+  ]
+};
